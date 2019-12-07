75d037853a66605bd4ae782cff578bd0890087a6
fsadannn <fsadannn@gmail.com>
2019-12-07 10:00:12 -0500

    build cli and gui in one setup, refactor cli because with fire all package are include

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ base = None
 if sys.platform == 'win32':
     base = 'Win32GUI'
 
+base2 = None
+if sys.platform == 'win32':
+    base2 = 'Console'
+
 options = {
     'build_exe': {
         'includes': ['atexit','nauta'],
-        'packages': ['bs4', 'lxml','requests', 'fire', 'lockfile','queue','dbm','pyqt5'],
+        'packages': ['bs4', 'lxml','requests', 'lockfile','queue','dbm','pyqt5','pipes','termcolor'],
         'include_msvcr': True,
         'optimize': 2
     }
@@ -17,12 +21,14 @@ options = {
 
 executables = [
     Executable('nauta_gui.py', base=base,
-               targetName='nauta_gui.exe',)
+               targetName='nauta_gui.exe',),
+    Executable('main.py', base=base2,
+               targetName='nauta_cli.exe',)
 ]
 
-setup(name='nauta_cli',
+setup(name='Nauta',
       version='1.0.1',
-      description='gui for control nauta connection and accounts',
+      description='gui and cli for control nauta connection and accounts',
       options=options,
       executables=executables
       )
